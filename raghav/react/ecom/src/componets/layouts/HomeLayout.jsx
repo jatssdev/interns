affd229db0cfe97cabf9d796f5c4337559e99295
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-07-02 13:45:58 +0530

    added gitignore

--- raghav/react/ecom/src/componets/layouts/HomeLayout.jsx
+++ raghav/react/ecom/src/componets/layouts/HomeLayout.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const HomeLayout = () => {
+    let [products, setProducts] = useState([])
+
+    let getProducts = async () => {
+        let result = await axios.get('http://localhost/amisha/api/getProducts.php')
+        console.log(result)
+        setProducts(result.data)
+
+    }
+
+
+    useEffect(() => {
+        getProducts()
+    }, [])
     return (
         <div>
-            Home layout
+            <h1>Products from php api</h1>
+            <div className="wrapper">
+                {
+                    products.map((product, index) => {
+
+                        return <>
+                            <h1>{product.title}</h1>
+                            <img src={product.img} alt="" />
+                        </>
+                    })
+                }
+            </div>
+
+
 
         </div>
     )
