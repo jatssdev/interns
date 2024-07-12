bc24a189f10104d503333c2bbfbd9fee2972c280
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-07-12 11:22:58 +0530

    delete and update

--- raghav/mern/backend/index.js
+++ raghav/mern/backend/index.js
@@ -5,45 +5,71 @@ app.use(cors())
 app.use(express.json())
 let User = require('./userModel')
 require('./db')
-
-
+let bcrypt = require('bcryptjs')
 app.get('/', (req, res) => {
     res.status(200)
     res.send('<h1>hlow</h1>')
 })
+app.get('/users', async (req, res) => {
+    let users = await User.find();
+    res.send(users)
 
+
+})
 app.get('/about', (req, res) => {
     res.status(200)
     res.send('<h1>hlow about</h1>')
 })
-
 app.get('/products', (req, res) => {
     res.status(200)
-    res.send(arr)
 })
-
 app.post('/register', async (req, res) => {
     let existingUser = await User.findOne({ email: req.body.email })
     if (existingUser) {
         res.send({ success: false, message: 'user already exists!' })
     } else {
+        let hashedpass = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedpass)
         let newUser = User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: hashedpass
         })
         let result = await newUser.save()
         if (result) {
-
-            res.send({ success: true, message: 'user registered sccessfully', user: result })
+            let { password, ...secureUSer } = result.toObject()
+            res.send({ success: true, message: 'user registered sccessfully', user: secureUSer })
         } else {
             res.send({ success: false, message: 'Error : database error ' })
 
         }
     }
-
+})
+app.post('/login', async (req, res) => {
+    let user = await User.findOne({ email: req.body.email })
+    if (user) {
+        let isMatch = await bcrypt.compare(req.body.password, user.password)
+        if (isMatch) {
+            res.send({ success: true, message: 'Login Successfully', user })
+        } else {
+            res.send({ success: false, message: 'password do not match' })
+        }
+    } else {
+        res.send({ success: false, message: 'invalid email' })
+    }
 })
 
+app.delete('/user/delete/:id', async (req, res) => {
+    let result = await User.findByIdAndDelete(req.params.id)
+    if (result)
+        res.send({ success: true, message: 'user deleted successfully' })
+})
+app.put('/user/edit/:id', async (req, res) => {
+    let result = await User.findByIdAndUpdate(req.params.id, { name: req.body.name, email: req.body.email })
+    if (result) {
+        res.send({ success: true, message: 'user updated successfully' })
+    }
+})
 app.listen(9000, () => {
     console.log('surver is running on 9000')
 })
--- raghav/mern/frontend/src/comps/Home.jsx
+++ raghav/mern/frontend/src/comps/Home.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+let Model = ({ userVAl, getAllUsers, setCurUSer }) => {
+    let [user, setUser] = useState({ name: '', email: '' })
+    const EditUSer = async (e) => {
+        e.preventDefault()
+        let result = await axios.put(`http://localhost:9000/user/edit/${userVAl._id}`, user)
+        if (result.data.success) {
+            setCurUSer({})
+            alert(result.data.message)
+            getAllUsers()
+        }
+    }
+    useEffect(() => {
+        setUser(userVAl)
+    }, [])
+    return (
+        <>
+            <div className="bg-gray-400 fixed w-1/2 top-1/2 p-12 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <form onSubmit={EditUSer} action="">
+                    <div className='my-3 '>
+                        <input className='p-2 border w-full' onChange={(e) => setUser({ ...user, name: e.target.value })} type="text" name={'name'} value={user.name} />
+                    </div>
+                    <div className='my-3 '>
+                        <input className='p-2 border w-full' onChange={(e) => setUser({ ...user, email: e.target.value })} type="text" name='email' value={user.email} />
+                    </div>
+                    <div className='my-3 '>
+                        <input className='p-2 border bg-blue-400' type="submit" value="Change" />
+                    </div>
+                </form>
+            </div>
+        </>
+    )
+}
 const Home = () => {
+    let [curUser, setCurUSer] = useState({
+        name: '', email: ''
+    })
+    let [users, setUSers] = useState([])
+
+    let getAllUsers = async () => {
+        let result = await axios.get('http://localhost:9000/users')
+        console.log(result.data)
+        setUSers(result.data)
+    }
+
+    useEffect(() => {
+        getAllUsers()
+    }, [])
+
+    const DeleteUser = async (id) => {
+        let result = await axios.delete(`http://localhost:9000/user/delete/${id}`)
+        if (result.data.success) {
+            alert(result.data.message)
+            getAllUsers()
+        }
+    }
+
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className='text-center text-2xl w-1/2 py-5'>users</h1>
+            <div className="wrapper">
+                {
+                    users.map((user, index) => {
+                        return <div key={index} className="flex justify-between w-1/2 shadow p-4 my-2">
+                            <h3>{index + 1} : {user.name}</h3>
+                            <div>
+                                <button className='bg-red-400 mx-2 px-3 py-2 rounded-md text-white' onClick={() => DeleteUser(user._id)}>Delete</button>
+                                <button onClick={() => setCurUSer(user)} className='bg-blue-400 mx-2 px-3 py-2 rounded-md text-white'>Edit</button>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+            {
+                curUser.name && <Model getAllUsers={getAllUsers} setCurUSer={setCurUSer} userVAl={curUser} />
+            }
         </div>
     )
 }
