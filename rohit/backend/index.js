10070dbf5328d84d030cacea5b59ce1c54dcb6a7
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-07-20 13:56:39 +0530

    crud

--- rohit/backend/index.js
+++ rohit/backend/index.js
@@ -11,12 +11,10 @@ app.get('/', (req, res) => {
     res.send('<h1>hello world</h1>')
 })
 
-
 app.get('/about/:email/:password', (req, res) => {
     console.log(req.params)
     res.send('<h1>' + req.params['email'] + '</h1>')
 })
-
 
 app.get('/products', (req, res) => {
     res.send(['jatin', 'magan', 'chhagan', 'lagan'])
@@ -43,6 +41,44 @@ app.post('/register', async (req, res) => {
     }
 })
 
+app.get('/users', async (req, res) => {
+    let users = await User.find()
+    res.send(users)
+})
+
+app.delete('/delete/:id', async (req, res) => {
+    // http://localhost:8000/delete/change_this_id
+    let result = await User.findByIdAndDelete(req.params.id)
+    if (result) {
+        res.send({
+            message: 'user deleted',
+            success: true
+        })
+    } else {
+        res.send({
+            message: 'user not deleted',
+            success: false
+        })
+    }
+
+})
+
+app.put('/update/:id', async (req, res) => {
+    // http://localhost:8000/update/change_this_id
+    let result = await User.findByIdAndUpdate(req.params.id, { email: req.body.email, name: req.body.name })
+    if (result) {
+        res.send({
+            message: 'user Updated',
+            success: true
+        })
+    } else {
+        res.send({
+            message: 'user not Updated',
+            success: false
+        })
+    }
+
+})
 
 app.listen(8000, () => console.log('your app is running on http://localhost:8000'))
 
@@ -51,3 +87,5 @@ app.listen(8000, () => console.log('your app is running on http://localhost:8000
 // npm i express cors
 
 
+// npm i bcryptjs
+// npm i mongoose 
