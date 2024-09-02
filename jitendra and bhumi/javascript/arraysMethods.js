f34aae4d8ec85ed9040dc6bbfbba6421103c20de
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-09-02 16:39:48 +0530

    ..

--- jitendra and bhumi/javascript/arraysMethods.js
+++ jitendra and bhumi/javascript/arraysMethods.js
@@ -2,6 +2,8 @@ let arr = [43242342, 3, 32, 542, 154, 23, 6523, 4, 124, 25, 324532, 4]
 let arr2 = [46543, 5, 4, 12, 4332, 634, 7543, 543]
 
 let newArr = [...arr, ...arr2] //[43242342, 3, 32, 542, 154, 23, 6523, 4, 124, 25, 324532, 4,46543, 5, 4, 12, 4332, 634, 7543, 543]
-console.log(newArr)
+// console.log(newArr)
 let output = arr.find((x) => x == 5)
+let str = 'helo my name is jatin'
+output = str.includes('jatin')
 console.log(output)
