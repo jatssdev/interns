d6856790f17a610c03be23684ec7e97a036cca2a
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-09-03 17:34:47 +0530

    .

---
+++ tej/first/src/App.jsx
@@ -0,0 +1,19 @@
+
+import About from './About'
+import './App.css'
+import JKaiBi from './assets/react.svg'
+import Header from './Header'
+import Home from './Home'
+function App() {
+
+  let name = 'tej'
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+    </>
+  )
+}
+
+export default App
---
+++ tej/first/src/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <>
+            <div className="navbar">
+                <p>Home</p>
+                <p>About</p>
+                <p>Contact</p>
+            </div>
+        </>
+    )
+}
+
+export default Header
