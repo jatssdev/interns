f37c3f8df805fb505668911ee3023b7958084c68
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-09-10 11:02:05 +0530

    ..

--- daxesh/node/index.js
+++ daxesh/node/index.js
@@ -1,8 +1,11 @@
+
 let express = require('express') // same as import express from 'express'
 let app = express()
 let cors = require('cors')
 app.use(cors())
-
+app.use(express.json())
+require('./conn')
+let User = require('./User')
 
 app.get('/', (req, res) => {
     res.send('helo wolrd')
@@ -13,9 +16,22 @@ app.get('/products', (req, res) => {
     res.send(products)
 })
 
+app.post('/register', async (req, res) => {
+    let newUser = User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
 
 
+    })
 
+    let result = await newUser.save()
+    if (result) {
+        res.send('user created')
+    } else {
+        res.send('user not created')
+    }
+})
 
 
 app.listen(9000, () => {
