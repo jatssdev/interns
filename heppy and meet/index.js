62c00c5a8055f7905da5f5f0691fd6a6b94939e5
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-09-23 17:29:01 +0530

    .

---
+++ heppy and meet/index.js
@@ -0,0 +1,26 @@
+function change() {
+    let heading = document.getElementById('heading')
+    heading.innerHTML = 'Coding Cloud'
+    heading.style.color = 'red'
+}
+function Dark() {
+
+    document.body.style.backgroundColor = 'black'
+    document.body.style.color = 'white'
+}
+
+function Light() {
+    document.body.style.backgroundColor = 'white'
+    document.body.style.color = 'black'
+}
+function On() {
+    document.getElementById('image').src = 'https://www.w3schools.com/js/pic_bulbon.gif'
+}
+function Off() {
+    document.getElementById('image').src = 'https://www.w3schools.com/js/pic_bulboff.gif'
+}
+let number = 0
+function Slide() {
+    number = number + 100
+    document.getElementById('box').style.transform = 'translate(' + number + 'px)'
+}
