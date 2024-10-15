d4284aaf10923d4c0c6ff88881644dbe9a0174be
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-10-15 16:37:02 +0530

    .

--- priyanshu/backend/index.js
+++ priyanshu/backend/index.js
@@ -7,6 +7,7 @@ let app = express()
 app.use(express.json())
 app.use(cors())
 require('./conn')
+let bcrypt = require('bcryptjs')
 
 let User = require('./userModel')
 
@@ -17,8 +18,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/register', async (req, res) => {
+    let hash = await bcrypt.hash(req.body.password, 10)
     let user = User({
-        name: req.body.name, email: req.body.email, password: req.body.password
+        name: req.body.name, email: req.body.email, password: hash
     })
     let existing = await User.findOne({ email: req.body.email })
     if (existing) {
@@ -30,6 +32,33 @@ app.post('/register', async (req, res) => {
         } else {
             res.send('Database Error : user not registered')
         }
+    }
+})
+
+app.post('/login', async (req, res) => {
+    let { email, password } = req.body
+
+    let user = await User.findOne({ email: email })
+    if (user) {
+        let checkPass = await bcrypt.compare(password, user.password)
+        if (checkPass) {
+            res.send({ user: user, message: 'login success!', success: true })
+        } else {
+            res.send('wrong password')
+        }
+    } else {
+        res.send('user not found')
+    }
+
+})
+
+app.delete('/user/:id', async (req, res) => {
+    let id = req.params.id
+    let result = await User.findByIdAndDelete(id)
+    if (result) {
+        res.send('user deleted successfully')
+    } else {
+        res.send('Database Error : user not deleted')
     }
 })
 
