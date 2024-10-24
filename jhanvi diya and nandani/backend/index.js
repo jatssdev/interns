dbc21953a8956fdab493e3786df488114d581620
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-10-24 15:12:06 +0530

    .

--- jhanvi diya and nandani/backend/index.js
+++ jhanvi diya and nandani/backend/index.js
@@ -7,7 +7,9 @@
 
 let express = require('express')
 let app = express()
-
+app.use(express.json())
+require('./conn')
+let User = require('./userModel')
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>') //send response to server
 })
@@ -71,7 +73,19 @@ app.get('/user', (req, res) => {
     })
 })
 
-
+app.post('/register', async (req, res) => {
+    let user = User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    let result = await user.save()
+    if (result) {
+        res.send('user registered successfully')
+    } else {
+        res.send('user not registered')
+    }
+})
 
 app.listen(8000, () => {
     console.log('server is running on port 8000')
---
+++ jhanvi diya and nandani/backend/userModel.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose')
+
+
+let userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+})
+let User = mongoose.model('users', userSchema)
+
+
+module.exports = User
